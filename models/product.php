e0a713a4cb5a55d1059ade5a21f735e3a64fa4ac
nqv2110 <88937231+nqv1710@users.noreply.github.com>
2021-11-26 20:22:47 +0700

    viet-active

--- models/product.php
+++ models/product.php
@@ -112,6 +112,17 @@
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items; //return an array
     }
+    //Lấy sản phẩm mới nhất theo Type_id - Việt
+    public function getProductsByType($type_id)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE type_id = ? 
+        ORDER BY `products`.`id` DESC");
+        $sql->bind_param("i", $type_id);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
   
 
 }
